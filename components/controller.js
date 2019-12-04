aaac9f137312504f3fba33f518a78c0e3f94722b
screeeen <miguel@screenimplosion.com>
2019-12-04 16:12:08 +0100

    test oculus go controls

---
+++ components/controller.js
@@ -0,0 +1,6 @@
+// `click` event emitted by browser on mouse click.
+document.querySelector('teleHand').addEventListener('triggerDown', function (evt) {
+
+  var bolaPiloti = querySelector('bola').setAttribute('color', '#ff0000');
+
+});
