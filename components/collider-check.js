94093fa761fd01beb8d49bbed5f7b1bd6c97d743
screeeen <miguel@screenimplosion.com>
2021-01-12 14:34:09 +0100

    amendments

--- components/collider-check.js
+++ components/collider-check.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('collider-check', {
   dependencies: ['raycaster'],
 
   init: function () {
-    this.el.addEventListener('raycaster-intersection', function () {
+    this.el.addEventListener('raycaster-intersection', function (evt) {
       console.log('Player hit something!');
     });
   }
